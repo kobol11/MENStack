4f0eb5a937265f8eb0aefa8c669f3297d31c9917
Bolarinwa Komolafe <Bolarinwa Komolafe>
2017-07-03 22:04:17 -0400

    sequelize and pg/pg-hstore modules added

--- data-service.js
+++ data-service.js
@@ -16,32 +16,14 @@ module.exports.getMessage = () => {
             reject("Oh No! Sorry your request cannot be completed");
         }
     });
-}
+};
 
 
 
 // This function reads the content of the "./data/employees.json" file
 module.exports.initialize = ()=>{
     return new Promise((resolve, reject)=>{
-        fs.readFile('./data/employees.json', (err, data)=>{
-            if (err){
-                reject("unable to read file");
-            }
-            else 
-            employees = JSON.parse(data);
-            empCount = employees.length;
-            resolve(fs.readFile('./data/departments.json', (err, data)=>{
-            if (err){
-                reject("unable to read file");
-            }
-            else 
-            departments = JSON.parse(data);
-            resolve();
-        }));
-        });
-
-        
-            
+        reject();
     });
 };
 
@@ -49,12 +31,7 @@ module.exports.initialize = ()=>{
 // This function will provide full array of employee objects using the resolve method of the returned promise
 module.exports.getAllEmployees = ()=>{
     return new Promise((resolve, reject)=>{
-        if(employees.length > 0){
-            resolve(employees);
-        }
-        else{
-            reject("no results returned");
-        }
+        reject();
     });
 };
 
@@ -62,40 +39,15 @@ module.exports.getAllEmployees = ()=>{
 // using the resolve method of the returned promise
 module.exports.getEmployeesByStatus = (status)=>{
     return new Promise((resolve, reject)=>{
-        var result = [];
-    
-            for (var i = 0; i < employees.length; i++){
-                if(employees[i].status == status){
-                    result.push(employees[i]);
-                }
-            }
-        if(result.length > 0)
-        resolve(result);
-    
-    else{
-        reject("no results returned");
-    }
-
-            });
+        reject();
+    });
 };
 
 // This function will provide an array of employee objects whose "department" property matches the department
 // parameter using the resolve method of the returned promise
 module.exports.getEmployeesByDepartment = (department)=>{
     return new Promise((resolve, reject)=>{
-        var result = [];
-    
-        for (var i = 0; i < employees.length; i++){
-            if (employees[i].department == department){
-                result.push(employees[i]);
-            }
-        }
-        if(result.length > 0)
-        resolve(result);
-    
-    else{
-        reject("no results returned");
-    }
+        reject();
     });
 };
 
@@ -103,19 +55,7 @@ module.exports.getEmployeesByDepartment = (department)=>{
 // "manager" parameter using the resolve method of the returned promise
 module.exports.getEmployeesByManager = (manager) =>{
     return new Promise((resolve, reject)=>{
-         var result = [];
-        
-             for (var i = 0; i < employees.length; i++){
-                 if(employees[i].employeeManagerNum == manager){
-                     result.push(employees[i]);
-                 }
-             }
-             if (result.length > 0)
-             resolve(result);
-        
-         else{
-               reject("no results returned");
-         }
+        reject();
     });
 };
 
@@ -123,19 +63,7 @@ module.exports.getEmployeesByManager = (manager) =>{
 // "num" parameter using the resolve method of the returned promise
 module.exports.getEmployeeByNum = (num) =>{
     return new Promise((resolve, reject)=>{
-        var result = [];
-        
-             for (var i = 0; i < employees.length; i++){
-                 if(parseInt(employees[i].employeeNum) == parseInt(num)){
-                     result.push(employees[i]);
-                 }
-             }
-             if (result.length > 0)
-             resolve(result);
-         
-         else{
-               reject("no results returned");
-         }
+        reject();
     });
 };
 
@@ -143,18 +71,7 @@ module.exports.getEmployeeByNum = (num) =>{
 // the resolve method of the returned promise
 module.exports.getManagers = ()=>{
     return new Promise((resolve, reject)=>{
-        var result = [];
-        for (var i = 0; i < employees.length; i++){
-            if(employees[i].isManager){
-                result.push(employees[i]);
-            }
-        }
-        if (result.length > 0)
-             resolve(result);
-         
-         else{
-               reject("no results returned");
-         }
+        reject();
     });
 };
 
@@ -163,37 +80,20 @@ module.exports.getManagers = ()=>{
 // returned promise
 module.exports.getDepartments = ()=>{
     return new Promise((resolve, reject)=>{
-        var result = [];
-    for (var i = 0; i < departments.length; i++){
-        result.push(departments[i]);
-    }
-        if (result.length > 0)
-             resolve(result);
-         
-         else{
-               reject("no results returned");
-         }
+        reject();
     });
 };
 
 // This function helps in adding new employee to the employee database
 module.exports.addEmployee = (employeeData)=>{
 return new Promise((resolve, reject)=>{
-    empCount++;
-    employeeData.employeeNum = empCount;
-    employees.push(employeeData);
-    resolve();
-});
+        reject();
+    });
 };
 
 // This function helps in updating the employee database
 module.exports.updateEmployee = (employeeData)=>{
     return new Promise((resolve, reject)=>{
-        for (var i = 0; i < employees.length; i++){
-            if (employees[i].employeeNum == employeeData.employeeNum){
-                employees[i] = employeeData;
-            }
-        }
-        resolve();
+        reject();
     });
 };

3307be7d6d4f095ee0c4002932a45be8877d0f58
Bolarinwa Komolafe <Bolarinwa Komolafe>
2017-07-15 21:04:15 -0400

    Schema created

--- data-service-comments.js
+++ data-service-comments.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 var contentSchema = new Schema ({
-    authorName: String,
-    authorEmail: String,
-    subject: String,
-    commentText: String,
-    postedDate: Date,
-    replies: [{
-        comment_id: String,
-        authorName: String,
-        authorEmail: String,
-        commentText: String,
-        repliedDate: Date
+    "authorName": String,
+    "authorEmail": String,
+    "subject": String,
+    "commentText": String,
+    "postedDate": Date,
+    "replies": [{
+        "comment_id": String,
+        "authorName": String,
+        "authorEmail": String,
+        "commentText": String,
+        "repliedDate": Date
     }]
 });
